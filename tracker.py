e2b50e27a71605a6ec11984bcdbf9f4363d099c8
Mateus Vilanova dos Passos <mate01vp@gmail.com>
2025-02-10 20:51:02 -0300

    fix: fixing the id on the add function. add get_id function

--- tracker.py
+++ tracker.py
@@ -7,6 +7,13 @@ commands_list  =["add", "delete", "update", "list", "mark-in-progress", "mark-do
 #create the task dictionary
 tasks = {}
 
+#get_id: return the last id + 1 in the tasks dictionary or 1 if the dictionary is empty
+def get_id():
+    if len(tasks):
+        return int(list(tasks)[-1]) + 1
+    else:
+        return 1
+
 #add task: create a new task with to-do status, set timestamp and update the task dictionary
 def add_task(id, description):
     task = {"description": description,
@@ -38,7 +45,7 @@ tasks = get_tasks()
 if sys.argv[1] not in commands_list:
     print("nao pegou comando")
 elif sys.argv[1] == commands_list[0]:
-    add_task(len(tasks) + 1, sys.argv[2])
+    add_task(get_id(), sys.argv[2])
     save_tasks()
 elif sys.argv[1] == commands_list[1]:
     delete(sys.argv[2])
